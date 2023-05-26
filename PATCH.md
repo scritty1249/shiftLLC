60df9693974ea2895835b8f3ffff7c4015182d19
Home Laptop <kaisonpham@gmail.com>
2023-05-26 03:35:45 -0700

    Ninth update: Finished carousel, index page almost done

--- PATCH.md
+++ PATCH.md
@@ -1,27 +1,25 @@
-### Eighth update patch notes
+### Ninth update patch notes
+
 # Additions
-- Button animations (bouncy responsiveness)
-- Placeholder CSS for carousel
-- Contact / Socials section at the bottom of every page
-- Contact / Socials styles
-- Index page layout
-- styles that pretain to the index page
-- Filled in text for index page
-- New padding and universal page layout margins
+
+-   Carousel
+-   Fixed mobile layout
+-   Events section of index page
+
 # Removed
-- Placeholder index page contents
-- old individual page padding layouts
+
+none
+
 # Next steps
-- Create a color wheel tool that allows management to explore color themes on the website remotely
-- make carousel functional
-    - Add javascript for carousel
-- Implement new layout for contact.htm page (possibly merge contact and about pages)
-    - Fill in text
-    - Add proposed card animations
-- Tidy up
+
+-   Create a color wheel tool that allows management to explore color themes on the website remotely
+-   Implement new layout for contact.htm page (possibly merge contact and about pages)
+    -   Fill in text
+    -   Add proposed card animations
+-   Tidy up
+
+~~make carousel functional~~
 
 ~~Finalize layout of index page~~
 
 ~~Create more button and menu CSS classes amd animations~~
-
-~~Test more screen sizes for possible font and layout errors~~
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,141 @@
-// Loading function
-$(document).ready(function() {
-    setTimeout(function(){
-        $('body').addClass('loaded');
-    }, 2000); // <-- Artificially make the page "load" to display loading animation in demo
- 
-});
-
 // Navigation bar
 /* Toggle between adding and removing the "responsive" class to the navbar when the user clicks on the icon */
 function expandNavMenu() {
     var navbar = document.getElementById("navbar");
     if (navbar.className === "navbar") {
-      navbar.className += " responsive";
+        navbar.className += " responsive";
     } else {
-      navbar.className = "navbar";
+        navbar.className = "navbar";
     }
-  }
+}
+
+// Carousel
+Carousel = {
+    $first_slide: null,
+    $last_slide: null,
+    slide_interval: null,
+    carousel_rotate_interval: NaN,
+    transition_duration: 2000,
+    updateSlides: function () {
+        this.$first_slide = $("#carousel").find(".slide:first");
+        this.$last_slide = $("#carousel").find(".slide:last");
+        console.log(this);
+        $("#carousel > #slide-buttons")
+            .find("button")
+            .css({
+                "-webkit-transform": "scale(" + 1 + ")",
+                "-moz-transform": "scale(" + 1 + ")",
+                "-ms-transform": "scale(" + 1 + ")",
+                "-o-transform": "scale(" + 1 + ")",
+                transform: "scale(" + 1 + ")",
+            });
+        $("#carousel > #slide-buttons")
+            .find("button")
+            .eq(parseInt(this.$first_slide.data("slide")) - 1)
+            .css({
+                "-webkit-transform": "scale(" + 1.5 + ")",
+                "-moz-transform": "scale(" + 1.5 + ")",
+                "-ms-transform": "scale(" + 1.5 + ")",
+                "-o-transform": "scale(" + 1.5 + ")",
+                transform: "scale(" + 1.5 + ")",
+            });
+    },
+    rotateSlides: function () {
+        this.$first_slide = $("#carousel").find(".slide:first");
+        this.$last_slide = $("#carousel").find(".slide:last");
+        this.$first_slide.animate(
+            { marginLeft: -this.$first_slide.width() },
+            this.transition_duration,
+            () => {
+                this.$last_slide.after(this.$first_slide);
+                this.$first_slide.css({ marginLeft: 0 });
+                this.$first_slide = $("#carousel").find(".slide:first");
+                this.$last_slide = $("#carousel").find(".slide:last");
+                $("#carousel > #slide-buttons")
+                    .find("button")
+                    .css({
+                        "-webkit-transform": "scale(" + 1 + ")",
+                        "-moz-transform": "scale(" + 1 + ")",
+                        "-ms-transform": "scale(" + 1 + ")",
+                        "-o-transform": "scale(" + 1 + ")",
+                        transform: "scale(" + 1 + ")",
+                    });
+                $("#carousel > #slide-buttons")
+                    .find("button")
+                    .eq(parseInt(this.$first_slide.data("slide")) - 1)
+                    .css({
+                        "-webkit-transform": "scale(" + 1.5 + ")",
+                        "-moz-transform": "scale(" + 1.5 + ")",
+                        "-ms-transform": "scale(" + 1.5 + ")",
+                        "-o-transform": "scale(" + 1.5 + ")",
+                        transform: "scale(" + 1.5 + ")",
+                    });
+            }
+        );
+    },
+    stopInterval: function () {
+        window.clearInterval(this.slide_interval);
+        this.updateSlides();
+    },
+    startInterval: function () {
+        this.updateSlides();
+        setTimeout(function () {}, 500); // Extra pause upon restarting the loop
+        this.slide_interval = window.setInterval(
+            this.rotateSlides,
+            this.carousel_rotate_interval
+        );
+    },
+    jumpToSlide: function (idx) {
+        window.clearInterval(this.slide_interval);
+        while ($("#carousel").find(".slide").eq(1).data("slide") != idx + 1) {
+            console.log(this.$first_slide.data("slide") + "...");
+            this.$last_slide.after(this.$first_slide);
+            this.$first_slide = $("#carousel").find(".slide:first");
+            this.$last_slide = $("#carousel").find(".slide:last");
+        }
+        this.$first_slide.animate(
+            { marginLeft: -this.$first_slide.width() * 2 },
+            this.transition_duration,
+            () => {
+                this.$last_slide.after(this.$first_slide);
+                this.$first_slide.css({ marginLeft: 0 });
+                this.startInterval();
+            }
+        );
+    },
+    activate: function (interval) {
+        if (this.$first_slide === null) {
+            $("#carousel")
+                .find(".slide")
+                .each(function () {
+                    $(this).css(
+                        "background-image",
+                        "url(" + $(this).data("image-slide") + ")"
+                    );
+                    $(this).data("image-slide", "");
+                });
+            this.carousel_rotate_interval = interval;
+            this.startInterval();
+            this.updateSlides();
+        }
+        $("#carousel > #slide-buttons")
+            .find("button")
+            .each((idx, e) => {
+                $(e).on("click", () => {
+                    this.jumpToSlide(idx);
+                });
+            });
+    },
+};
+
+// Loading function
+$(document).ready(function () {
+    setTimeout(function () {
+        // Reveal the page
+        $("body").addClass("loaded");
+        // Load carousel
+        if ($("#carousel").length > 0) {
+            Carousel.activate(15 * 1000);
+        }
+    }, 2000); // <-- Artificially make the page "load" to display loading animation in demo
+});
